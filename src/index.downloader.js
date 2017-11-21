edee9e705969f0e666427cc52539f378bf61c63d
谢庭 <xieting@xietingdeMini.lan>
2017-11-21 15:51:11 +0800

    add .npmignore

--- src/index.downloader.js
+++ src/index.downloader.js
@@ -15,4 +15,35 @@ function PearDownloader(urlStr, token, opts) {
     // if (!(self instanceof PearPlayer)) return new PearPlayer(selector, opts);
     if (typeof token === 'object') return PearDownloader(urlStr, '', token);
     Worker.call(self, urlStr, token, opts);
+
+
 }
+
+var PRDownloaderProto = Object.create(HTMLElement.prototype);
+PRDownloaderProto.createdCallback = function() {
+    console.log('XTreehouseProto created');
+}
+PRDownloaderProto.attachedCallback = function() {
+    console.log('XTreehouseProto attached:'+this.dataset.useMonitor);
+    console.log(this.dataset.src)
+    var downloader = new PearDownloader(this.dataset.src, {
+        scheduler: this.dataset.scheduler,
+        auto: this.dataset.auto,
+        interval: this.dataset.interval,
+        useDataChannel: this.dataset.useDataChannel,
+        dataChannels: this.dataset.dataChannels,
+        useTorrent: this.dataset.useTorrent,
+        magnetURI: this.dataset.magnetURI,
+        // trackers:["wss://tracker.openwebtorrent.com"],
+        // sources: [],
+        useMonitor: this.dataset.useMonitor,
+    });
+    PRDownloaderProto.downloader = downloader;
+    // downloader.call(this);
+}
+PRDownloaderProto.detachedCallback = function() {
+
+}
+PRDownloaderProto.attributeChangedCallback = function(attrName, oldValue, newValue) {}
+var PRDownloader = document.registerElement('pr-downloader', { prototype: PRDownloaderProto });
+// PRDownloader.call(PearDownloader);
